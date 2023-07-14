027e0cfc088bd9846a0fc3b9ad8428f0b89c75c5
Rapheal Jones <110284565+Bajpai-Kshitij@users.noreply.github.com>
2023-07-14 02:57:33 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import "./styles.css";
+import { useState } from "react";
 
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
     title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
     title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
 ];
 
 export default function App() {
@@ -27,5 +25,39 @@ export default function App() {
 }
 
 function Accordion() {
-  return <div>TODO</div>;
+  const [isClicked, setIsClicked] = useState(null);
+
+  function handleClick(index) {
+    setIsClicked(index);
+  }
+  return (
+    <div className="accordion">
+      {faqs.map((elem, index) => (
+        <Item
+          item={elem}
+          click={isClicked}
+          onClickItem={handleClick}
+          index={index}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Item({ item, click, onClickItem, index }) {
+  const clickBool = click === index;
+  return (
+    <div
+      className={clickBool ? "content-box open" : "content-box"}
+      onClick={() => onClickItem(index)}
+    >
+      <ul className="item">
+        <li className="title">
+          <span className="number">{index + 1}</span> {item.title}
+        </li>
+        <li className="text">{clickBool ? item.text : ""}</li>
+      </ul>
+    </div>
+  );
 }
